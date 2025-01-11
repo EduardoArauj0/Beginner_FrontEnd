3c800cadfdc978e0f8b3a4c8e533448bac3ae200
EduardoArauj0 <eduardoaraujo.contato13@gmail.com>
2025-01-11 00:30:54 -0300

    adição de novas funções

--- Repetições/while.js
+++ Repetições/while.js
@@ -1,10 +1,12 @@
 var contador = 1;
 
+// Teste Logico feito no inicio
 while (contador <= 10) {
   console.log(`Passo ${contador}`);
   contador++;
 }
 
+// Teste Logico feito no final
 do {
   console.log(`Passo ${contador}`);
   contador++;
