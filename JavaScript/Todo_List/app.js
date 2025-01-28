fa913dea3f299eca19d4f396b92c3ca4a04c437a
EduardoArauj0 <eduardoaraujo.contato13@gmail.com>
2025-01-27 21:04:10 -0300

    build: app.js (cria a const atualizarTela)

--- JavaScript/Todo_List/app.js
+++ JavaScript/Todo_List/app.js
@@ -1,1 +1,25 @@
 "use strict";
+
+let db = [
+  { texto: "Estudar JS", status: "" },
+  { texto: "Netflix", status: "checked" },
+  
+];
+
+const criarItem = (texto, status = "") => {
+  const item = document.createElement("label");
+  item.classList.add("todo__item");
+  item.innerHTML = ` 
+<input type="checkbox" ${status}>
+<div>${texto}</div>
+<input type="button" value="X"> 
+`;
+
+  document.getElementById("TodoList").appendChild(item);
+};
+
+const atualizarTela = () => {
+  db.forEach(item => criarItem (item.texto, item.status));
+};
+
+atualizarTela();
